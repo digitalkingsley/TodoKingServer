8709157398d8f0313049ca22c9d7405782c1d20d
Kingsley Okei <kingsleycokei@gmail.com>
2024-06-09 01:54:26 +0100

    Added .gitignore file and some properties to TKTasks POCO

---
+++ TodoKingServer/Models/TKTask.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoKingServer.Models
+{
+    public class TKTask
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public DateTime Created { get; set; }
+        public string Status { get; set; } = String.Empty;
+        public Guid TKUserId { get; set; }
+        public TKUser TKUser { get; set; }
+
+    }
+}
---
+++ TodoKingServer/Models/TKUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoKingServer.Models
+{
+    
+    public class TKUser
+    {
+        
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+        public IEnumerable<TKTask> Tasks { get; set; }
+    }
+}
